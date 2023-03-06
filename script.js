14291714412443e88d0e90b7ab55b2ec53b1ec2f
snobacho <giorgicheli@gmail.com>
2023-03-06 17:15:42 +0400

    added part of responsive navbar menu

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
+const container = document.querySelector('.container')
+
+// document.querySelector('.open-navbar').addEventListener('click', () => {
+//     container.classList.add('change')
+// })
+// document.querySelector('.close-navbar').addEventListener('click', () => {
+//     container.classList.remove('change')
+// })
+
+const colors = ['#6495ed', '#7fffd4','#6495ed','#7fffd4', '#6495ed' ]
+
+let i = 0
+
+Array.from(document.querySelectorAll('.nav-link'))
+.forEach((item) => {
+    item.style.cssText = `background-color: ${colors[i++]}`
+})
+
+
 Array.from(document.querySelectorAll('.navigation-button'))
 .forEach((item) => {
     item.onclick = () => {
         item.parentElement.parentElement.classList
         .toggle('change')
     }
-})
+}) 
