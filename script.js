17af68dcf8c4c82a5e6c9c396a7fe1c7ee23b8c9
snobacho <giorgicheli@gmail.com>
2023-03-07 23:40:40 +0400

    added navigation and scroll

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const container = document.querySelector('.container')
 
-document.querySelector('.open-navbar-inon').addEventListener('click', function() {
+document.querySelector('.open-navbar-inon').addEventListener('click', (e) => {
+    e.stopPropagation()
     container.classList.add('change')
 })
-document.querySelector('.close-navbar-icon').addEventListener('click', function() {
+document.querySelector('.close-navbar-icon').addEventListener('click', (e) => {
+    e.stopPropagation()
     container.classList.remove('change')
 })
 
+window.addEventListener('click', function(e) {      // * close navbar 
+    e.stopPropagation()
+    container.classList.remove('change')
+})
 
 const colors = ['#000B0B', '#065E64','#000B0B','#065E64', '#080500' ]
 
@@ -16,7 +22,6 @@ Array.from(document.querySelectorAll('.nav-link'))
 .forEach((item) => {
     item.style.cssText = `background-color: ${colors[i++]}`
 })
-
 
 Array.from(document.querySelectorAll('.navigation-button'))
 .forEach((item) => {
